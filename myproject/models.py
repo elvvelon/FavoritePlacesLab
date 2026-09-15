a973297a03bd3eb9941a4bc6090eaecdf4e58d64
Danylo Maister <mayster.danya@gmail.com>
2026-09-15 19:10:12 +0300

    feat: implement UI templates, forms, and final routing

--- myproject/models.py
+++ myproject/models.py
@@ -2,11 +2,11 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 class Place(models.Model):
-    session_key = models.CharField(max_length = 40)
+    session_key = models.CharField(max_length=40)
     name = models.CharField("Name", max_length=200)
     description = models.TextField("Description")
-    place_type = models.CharField("Type", max_length = 100)
-    address = models.CharField("Address", max_length = 200)
+    place_type = models.CharField("Type", max_length=100)
+    address = models.CharField("Address", max_length=200, blank=True, null=True)
     rating = models.IntegerField(
         "Rating",
         validators=[MinValueValidator(1), MaxValueValidator(5)]
@@ -17,7 +17,11 @@ class Place(models.Model):
         words = self.description.split(" ")
         if len(words) > 5:
             return " ".join(words[:5]) + "..."
-        else: return self.description
+        else:
+            return self.description
+
+    def get_rating(self):
+        return "★" * self.rating + "☆" * (5 - self.rating)
 
     def __str__(self):
         return self.name
--- myproject/views.py
+++ myproject/views.py
@@ -1,5 +1,6 @@
 import random
 from django.shortcuts import render, get_object_or_404, redirect
+from myproject.forms import PlaceForm
 from myproject.models import Place
 
 def get_session_key(request):
@@ -16,18 +17,28 @@ def home(request):
         weights = [place.rating for place in places]
         random_place = random.choices(list(places), weights=weights, k=1)[0]
 
-    return
+    return render(request, 'myproject/home.html', {'random_place': random_place})
 
 def places_list(request):
     session_key = get_session_key(request)
     places = Place.objects.filter(session_key=session_key).order_by('-created_at')
-    return
+    return render(request, 'myproject/places_list.html', {'places': places})
 
 def place_detail(request, place_id):
     session_key = get_session_key(request)
     place = get_object_or_404(Place, pk=place_id, session_key=session_key)
-    return
+    return render(request, 'myproject/place_detail.html', {'place': place})
 
 def place_add(request):
     session_key = get_session_key(request)
-    return
+    if request.method == 'POST':
+        form = PlaceForm(request.POST)
+        if form.is_valid():
+            place = form.save(commit=False)
+            place.session_key = get_session_key(request)
+            place.save()
+            return redirect('places_list')
+    else:
+        form = PlaceForm()
+
+    return render(request, 'myproject/place_add.html', {'form': form})
